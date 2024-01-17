a0c7949e31fe6b7d4d50918e68970ab0977142bf
ValeriaRybalska <vrybalskaa@gmail.com>
2024-01-17 12:40:51 +0200

    Add the file

---
+++ Autocode.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace epam_course
+{
+    static void Main ()
+    {
+        string str = "Hello world"
+        var a = 123
+
+        Console.WriteLine(str + a )
+    }
+}
---
+++ Program копія.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cs
+{
+    class Program
+    {
+        static void Main ()
+        {
+            int a = 1;
+            int b = 2;
+
+            Console.WriteLine (a + b);
+            Console.WriteLine("Hello World ");
+        }
+    }
+}
